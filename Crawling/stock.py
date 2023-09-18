8f6d491ddff05a9de2c77d3b86f5e0ac8bbda488
maxmin90 <91pp90@gmail.com>
2023-09-18 13:53:37 +0900

    완료

--- Crawling/stock.py
+++ Crawling/stock.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import datetime
 
 def print_stock_price(code, page_num):
     result = [[], [], [], [], [], [], []]
+    global stock 
+    stock = []
 
     for n in range(page_num):
         url = 'https://finance.naver.com/item/sise_day.nhn?code='+code+'&page='+str(n+1)
@@ -27,9 +28,31 @@ def print_stock_price(code, page_num):
         if  '2023.08.01' <= result[0][i]  <= '2023.08.31':
             print(result[0][i], result[1][i], result[2][i],result[3][i],result[4][i],result[5][i],result[6][i])
 
-
+            stock.append({
+                "날짜":result[0][i],
+                "종가": result[1][i],
+                "전일비":result[2][i],
+                "시가":result[3][i],
+                "고가":result[4][i],
+                "저가":result[5][i],
+                "거래량":result[6][i]
+                })
+            # print(stock) # 딕셔너리 형태로 출력 
 
 stock_code = '005930'
 pages = 4
 
-print_stock_price(stock_code, pages)
+print_stock_price(stock_code, pages) # 줄 글로 출력 
+
+import json
+stock
+
+with open("stock.json", "w", encoding="utf-8-sig") as f:
+    json.dump(stock, f, ensure_ascii = False)
+import csv
+with open("stock.json", "r", encoding="utf-8-sig") as input_file, open("stock.csv", "w", newline="") as output_file:
+    data = json.load(input_file)
+    f = csv.writer(output_file)
+    for datum in data:
+        f.writerow([datum["날짜"], datum["종가"], datum["전일비"], datum["시가"], datum["고가"], datum["저가"], datum["거래량"]])
+
