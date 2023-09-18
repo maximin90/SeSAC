e40167e5105dccc758dd1a3688b0026e56bb5b74
maxmin90 <91pp90@gmail.com>
2023-09-18 11:22:40 +0900

    완료

--- Crawling/stock.py
+++ Crawling/stock.py
@@ -2,17 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-# url = 'https://finance.naver.com/item/sise.nhn?code=005930'
-# response = requests.get(url)
-# soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup)
-
-# url = 'https://finance.naver.com//item/sise_day.naver?code=005930'
-# agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-# response = requests.get(url, headers = {'User-Agent': agent})
-# _soap = BeautifulSoup(response.text, 'html.parser')
-# response.status_code
-
 def print_stock_price(code, page_num):
     result = [[], [], [], [], [], [], []]
 
